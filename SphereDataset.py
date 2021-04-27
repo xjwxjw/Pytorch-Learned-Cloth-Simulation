8f3c58fe8c0890fc1155b9d9839d73dbcd4e27c2
xjwxjw <xjwxjw@sjtu.edu.cn>
2021-04-28 00:28:52 +0800

    Put all data into training

--- SphereDataset.py
+++ SphereDataset.py
@@ -15,10 +15,15 @@ def collate_fn(batch):
 
 class SphereDataset(Dataset):
     def __init__(self, data_dir, length):
-        self.state_dir = os.path.join(data_dir, '0002')
         self.data = []
-        for i in range(500):
-            self.data.append(os.path.join(self.state_dir, '%03d_cloth.txt' % i))
+        # self.seq = []
+        self.length = length
+        for data_id in range(100):
+            state_dir = os.path.join(data_dir, ('data_0424/%04d' % data_id))
+            for file_id in range(1, 499):
+                cur_file = os.path.join(state_dir, '%03d_cloth.txt' % file_id)
+                nxt_file = os.path.join(state_dir, '%03d_cloth.txt' % (file_id + 1))
+                self.data.append([cur_file, nxt_file])
         self.cloth_topo = np.load(os.path.join(data_dir, 'cloth_connection.npy'), allow_pickle = True)
         self.sphere_topo = np.load(os.path.join(data_dir, 'sphere_connection.npy'), allow_pickle = True)
         self.adj_map = np.load(os.path.join(data_dir, 'adj_map.npy'), allow_pickle = True)
@@ -33,14 +38,14 @@ class SphereDataset(Dataset):
         self.cloth_want_idx = [0,1,2, 9,10,11]#0-2:position, 9-11:velocity, 16-17:u-v coord
     
     def __len__(self):
-        return len(self.data) - 1
+        return len(self.data) 
 
-    def GetState(self, index):
+    def GetState(self, index, time_id):
         #### get current state, including vertex and edge information, as input ####
-        cloth_file = self.data[index]
-        ball_file = self.data[index].replace('cloth', 'ball')
-        uv_file = self.data[index].replace('cloth', 'uv')
-        world_file = self.data[index].replace('cloth', 'world')
+        cloth_file = self.data[index][0]
+        ball_file = self.data[index][0].replace('cloth', 'ball')
+        uv_file = self.data[index][0].replace('cloth', 'uv')
+        world_file = self.data[index][0].replace('cloth', 'world')
 
         cloth_data = []
         ball_data = []
@@ -69,7 +74,7 @@ class SphereDataset(Dataset):
         world_data = np.array(world_data)
         
         #### get next state, mainly the position, as output ####
-        cloth_nxt_file = self.data[index+1]
+        cloth_nxt_file = self.data[index][1]
         cloth_nxt_data = []
         for line in open(cloth_nxt_file, 'r'):
             line = line.split('\n')[0]
@@ -79,7 +84,8 @@ class SphereDataset(Dataset):
         return cloth_data, ball_data, uv_data, world_data, cloth_nxt_data
 
     def __getitem__(self, index):
-        cloth_state, ball_state, uv_state, world_state, cloth_nxt_state = self.GetState(index)
+        time_id = np.random.choice(self.length - 2, 1)[0] + 1
+        cloth_state, ball_state, uv_state, world_state, cloth_nxt_state = self.GetState(index, time_id)
         return torch.from_numpy(cloth_state[:, self.cloth_want_idx].astype(np.float32)),\
                torch.from_numpy(ball_state.astype(np.float32)), \
                torch.from_numpy(uv_state.astype(np.float32)),\
@@ -88,26 +94,38 @@ class SphereDataset(Dataset):
 
 if __name__ == '__main__':
     spdataset = SphereDataset('../Data', 500)
-    sploader = DataLoader(spdataset, batch_size = 2, shuffle = True, num_workers = 2, collate_fn = collate_fn)
-    for cloth_state, ball_state, uv_state, world_state, cloth_nxt_state in sploader:
-        print(cloth_state[0].shape, ball_state[0].shape, uv_state[0].shape, world_state[0].shape, cloth_nxt_state[0].shape)
-
-    # import plotly.graph_objects as go
-    # import numpy as np
-
-    # cloth_data = []
-    # for line in open('../Data/0002/005_cloth.txt', 'r'):
-    #     line = line.split('\n')[0]
-    #     cloth_data.append(np.array([float(data) for data in line.split(' ')[:-1]]))
-    # cloth_data = np.array(cloth_data)
-    # x, y, z = cloth_data[:,:3].T
-    # l = 0
-    # i, j, k = [], [], []
+    sploader = DataLoader(spdataset, batch_size = 2, shuffle = True, num_workers = 8, collate_fn = collate_fn)
+    for step, (cloth_state, ball_state, uv_state, world_state, cloth_nxt_state) in enumerate(sploader):
+        print(step, cloth_state[0].size(), world_state[0].size())
     # cloth_connection = np.array([int(line.split('\n')[0]) for line in open('../Data/cloth_connection.txt', 'r')])
     # cloth_connection = np.reshape(cloth_connection, (-1, 3))
-    # fig = go.Figure(data=[go.Mesh3d(x = x, y = y, z = z, \
-    #                                 i = cloth_connection[:, 0], \
-    #                                 j = cloth_connection[:, 1], \
-    #                                 k = cloth_connection[:, 2], \
-    #                                 color='lightpink', opacity=0.50)])
-    # fig.show()
+    # ball_connection = np.array([int(line.split('\n')[0]) for line in open('../Data/sphere_connection.txt', 'r')])
+    # ball_connection = np.reshape(ball_connection, (-1, 3))
+
+    # from mpl_toolkits.mplot3d import Axes3D as axes3d
+    # import matplotlib.pyplot as plt
+    
+    # for i in range(500):
+    #     cloth_data = []
+    #     for line in open('../Data/0002/%03d_cloth.txt' % i, 'r'):
+    #         line = line.split('\n')[0]
+    #         cloth_data.append(np.array([float(data) for data in line.split(' ')[:-1]]))
+    #     cloth_data = np.array(cloth_data)
+    #     cloth_x, cloth_y, cloth_z = cloth_data[:,:3].T
+
+    #     ball_data = []
+    #     for line in open('../Data/0002/%03d_ball.txt' % i, 'r'):
+    #         line = line.split('\n')[0]
+    #         ball_data.append(np.array([float(data) for data in line.split(' ')[:-1]]))
+    #     ball_data = np.array(ball_data)
+    #     ball_x, ball_y, ball_z = ball_data[:,:3].T
+
+    #     fig = plt.figure()
+    #     ax = fig.gca(projection = '3d')
+    #     ax.plot_trisurf(cloth_x, cloth_z, cloth_y, triangles = cloth_connection)
+    #     ax.plot_trisurf(ball_x, ball_z, ball_y, triangles = ball_connection)
+    #     ax.set(xlim=(-1, 1), ylim=(-1, 1), zlim=(-1, 1))
+    #     plt.savefig('../Results/%03d.png' % i)
+    #     # plt.show()
+    #     plt.close('all')
+    
--- Utils.py
+++ Utils.py
@@ -19,20 +19,22 @@ def GenDataStat():
         print('%s_mean:' % target, target_mean)
         print('%s_std:' % target, target_std + 1e-10)
 
-def GenEdgeFeature():
+import threadpool 
+
+def GenEdgeFeature(seq_id):
     cloth_connection = np.load('../Data/cloth_connection.npy', allow_pickle=True).item()    
     ball_connection = np.load('../Data/sphere_connection.npy', allow_pickle=True).item()    
     for i in range(500):
         print(i)
         cloth_data = []
-        cloth_path = ('../Data/0002/%03d_cloth.txt' % i)
+        cloth_path = ('../Data/data_0424/%04d/%03d_cloth.txt' % (seq_id, i))
         for line in open(cloth_path, 'r'):
             line = line.split('\n')[0]
             cloth_data.append(np.array([float(data) for data in line.split(' ')[:-1]]))
         cloth_data = np.array(cloth_data)
 
         ball_data = []
-        ball_path = ('../Data/0002/%03d_ball.txt' % i)
+        ball_path = ('../Data/data_0424/%04d/%03d_ball.txt' % (seq_id, i))
         for line in open(ball_path, 'r'):
             line = line.split('\n')[0]
             ball_data.append(np.array([float(data) for data in line.split(' ')[:-1]]))
@@ -58,7 +60,7 @@ def GenEdgeFeature():
                     edge_cnt += 1
             np.save('../Data/adj_map.npy', adj_map)
 
-        foutuv = open('../Data/0002/%03d_uv.txt' % i, 'w')
+        foutuv = open('../Data//data_0424/%04d/%03d_uv.txt' % (seq_id, i), 'w')
         for key in cloth_connection.keys():
             for val in cloth_connection[key]:
                 i_vertx = cloth_data[key]
@@ -71,7 +73,7 @@ def GenEdgeFeature():
                             (uij[0], uij[1], uij_norm, xij[0], xij[1], xij[2], xij_norm))
         foutuv.close()
 
-        foutworld = open('../Data/0002/%03d_world.txt' % i, 'w')
+        foutworld = open('../Data/data_0424/%04d/%03d_world.txt' % (seq_id, i), 'w')
         cloth_world_dis = np.sum((cloth_data[None, :, :3] - cloth_data[:, None, :3])**2, -1)**0.5
         ball_world_dis = np.sum((cloth_data[None, :, :3] - ball_data[:, None, :3])**2, -1)**0.5
         idxs_cloth = np.argwhere(cloth_world_dis < 0.015)
@@ -82,7 +84,6 @@ def GenEdgeFeature():
             xij = i_vertx[:3] - j_vertx[:3]
             xij_norm = np.linalg.norm(xij, ord = 2)
             if xij_norm < 0.015 and (idx[0] != idx[1]):
-                print('cloth:', idx[0], idx[1], xij[0], xij[1], xij[2], xij_norm)
                 foutworld.write("%d %d %.6f %.6f %.6f %.6f\n" % (idx[0], idx[1], xij[0], xij[1], xij[2], xij_norm))
         
         for idx in idxs_ball:
@@ -91,13 +92,14 @@ def GenEdgeFeature():
             xij = i_vertx[:3] - j_vertx[:3]
             xij_norm = np.linalg.norm(xij, ord = 2)
             if xij_norm < 0.015:
-                print('ball:', idx[1], idx[0], xij[0], xij[1], xij[2], xij_norm)
                 foutworld.write("%d %d %.6f %.6f %.6f %.6f\n" % (idx[1], idx[0], xij[0], xij[1], xij[2], xij_norm))
         foutworld.close()
 
-def GenWorldEdgeFeature():
-    pass
+import threading
+import multiprocessing
 
 if __name__ == "__main__":
-    GenEdgeFeature()
+    for i in range(100):
+        p1 = multiprocessing.Process(target = GenEdgeFeature, args = (i,))
+        p1.start()
 
